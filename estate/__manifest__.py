8e450cddfc0b1264a1b943be9215842f743415d8
Catmuf <>
2026-09-21 12:52:00 +0200

    Chapter 4: Security - A Brief Introduction

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,7 +10,9 @@
     'depends': [
         'base',
     ],
-    'data': [],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
