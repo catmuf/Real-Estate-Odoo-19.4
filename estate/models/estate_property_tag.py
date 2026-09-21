574d33bf5e178bf0d4742508b7a8a071f37ca442
Catmuf <>
2026-09-21 12:53:05 +0200

    Chapter 7: Relations Between Models

---
+++ estate/models/estate_property_tag.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class EstatePropertyTag(models.Model):
+    _name = "estate.property.tag"
+    _description = "Property Tag"
+
+    name = fields.Char(string="Name", required=True)
